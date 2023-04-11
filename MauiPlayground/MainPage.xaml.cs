cbd3ec53f57bc567139d74c4aeb73fe310fe5719
Bill Vo <Bill.Vo@CompanionSystems.com.au>
2023-04-11 14:31:00 +1000

    Add functionality on Main Page to call an entered number

--- MauiPlayground/MainPage.xaml.cs
+++ MauiPlayground/MainPage.xaml.cs
@@ -5,7 +5,8 @@ public partial class MainPage : ContentPage
 
     #region - - - - - - Fields - - - - - -
 
-    private int count = 0;
+    private int ClickCount = 0;
+    private string TranslatedNumber = "";
 
     #endregion Fields
 
@@ -20,16 +21,59 @@ public partial class MainPage : ContentPage
 
     #region - - - - - - Methods - - - - - -
 
+    async void OnCall(object sender, EventArgs e)
+    {
+        if (await DisplayAlert(
+                "Dial a Number",
+                $"Would you like to call {TranslatedNumber} ?",
+                "Yes",
+                "No"))
+        {
+            try
+            {
+                if (PhoneDialer.Default.IsSupported)
+                    PhoneDialer.Default.Open(TranslatedNumber);
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+            }
+        }
+    }
+
     private void OnCounterClicked(object sender, EventArgs e)
     {
-        count++;
+        ClickCount++;
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
+        if (ClickCount == 1)
+            CounterBtn.Text = $"Clicked {ClickCount} time";
+
         else
-            CounterBtn.Text = $"Clicked {count} times, when u gunna stop?";
+            CounterBtn.Text = $"Clicked {ClickCount} times, when u gunna stop?";
 
         SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        TranslatedNumber = PhonewordTranslator.ToNumber(TextInputPhoneNumber.Text);
+
+        if (!string.IsNullOrEmpty(TranslatedNumber))
+        {
+            ButtonToCallNumber.IsEnabled = true;
+            ButtonToCallNumber.Text = $"Call {TranslatedNumber}";
+            SemanticScreenReader.Announce(ButtonToCallNumber.Text);
+        }
+
+        else
+        {
+            ButtonToCallNumber.IsEnabled = false;
+            ButtonToCallNumber.Text = "Call";
+        }
     }
 
     #endregion Methods
